4c1fffa6223fc69fea06ac7cc1b0f54fc994a74b
buraksaler <burakksaler@gmail.com>
2022-11-02 11:23:05 +0300

    create and save document

--- tutorial.py
+++
@@ -1,9 +0,0 @@
-from pyArango.connection import *
-
-conn = Connection(username="root", password="12345")
-
-db = conn["mydev"]
-
-devCollection = db.createCollection(name="mydevCollection")
-print(db)
-print(db["mydevCollection"])
